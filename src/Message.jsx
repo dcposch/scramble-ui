5aa718c58e6b513794c3cf4e97684d34c985cec4
DC <dcposch@dcpos.ch>
2015-01-06 23:52:27 -0800

    Message UI

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,13 +2,34 @@ var React = require("react");
 
 module.exports = React.createClass({
   propTypes: {
-    "name": React.PropTypes.string
+    "from": React.PropTypes.string,
+    "to": React.PropTypes.array,
+    "body": React.PropTypes.string
   },
   render: function() {
     if(!this.props.name) {
       return (<div />);
     }
-    return (<h1>Hello, {this.props.name}</h1>);
+    return (
+      <div>
+        <div className="panel panel-default">
+            <div className="panel-heading">
+                <small className="received pull-right">{{formatDate time format="lll"}}</small>
+                <div className="from">
+                    {this.props.from}
+                </div>
+                <small className="to">to 
+                    {this.props.to.join(",")}
+                </small>
+                <small><a href="#" className="pull-right">Show Original</a></small>
+            </div>
+
+            <div id="body-{{hexMsgID}}" className="email-body panel-body {{#unless htmlBody}}still-decrypting{{/unless}}">{this.props.body}</div>
+            <div className="panel-body">{this.props.body}</div>
+        </div>
+
+        <div className="email-compose"></div>
+    </div>
   }
 });
 
